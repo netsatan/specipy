cf99f19d3514dac746935712ca8c99df4b2e1a30
netsatan <kaybeaver@gmail.com>
2024-04-30 01:56:14 +0200

    Extends D2HashableConnection's __eq__ method to also validate label

--- tests/test_diagram_engines.py
+++ tests/test_diagram_engines.py
@@ -10,3 +10,18 @@ class TestDiagramEngines(TestCase):
         connection_1 = D2HashableConnection("a", "b", "some_label", Direction.TO)
         connection_2 = D2HashableConnection("a", "b", "some_label", Direction.TO)
         assert connection_1 == connection_2
+
+    def test_connections_hash(self):
+        connection_1 = D2HashableConnection("a", "b", "some_label", Direction.TO)
+        assert isinstance(connection_1.__hash__(), int)
+
+    def test_connections_not_equal(self):
+        connection_1 = D2HashableConnection("a", "b", "some_label", Direction.TO)
+        connection_2 = D2HashableConnection("a", "b", "some_label2", Direction.TO)
+        connection_3 = D2HashableConnection("a1", "b", "some_label", Direction.TO)
+        connection_4 = D2HashableConnection("a", "b1", "some_label", Direction.TO)
+        connection_5 = D2HashableConnection("a", "b", "some_label", Direction.FROM)
+        assert connection_1 != connection_2
+        assert connection_1 != connection_3
+        assert connection_1 != connection_4
+        assert connection_1 != connection_5
