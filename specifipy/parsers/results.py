0885a3e48ad889646fb47f56d276b8bb48e368f7
netsatan <kaybeaver@gmail.com>
2024-05-02 02:31:43 +0200

    Optional Docstring field for parsing results. Not yet used by DiagramGenerator, but already understood by a parser, at least for Java code.

--- specifipy/parsers/results.py
+++ specifipy/parsers/results.py
@@ -1,7 +1,9 @@
 import dataclasses
+from typing import Optional
 
 from specifipy.parsers.structure.code_structure_definitions import (
     ClassStructureDefinition,
+    Docstring,
     FieldStructureDefinition,
     FunctionStructureDefinition,
 )
@@ -12,3 +14,4 @@ class ParsingResult:
     classes: list[ClassStructureDefinition]
     functions: list[FunctionStructureDefinition]
     class_fields: list[FieldStructureDefinition]
+    docstrings: Optional[list[Docstring]] = None
